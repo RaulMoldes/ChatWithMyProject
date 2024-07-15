7d74dea2fa9b6d9efa2f9cc34253f47fa81db659
RaulMoldes <raul.moldes.work@gmail.com>
2024-07-15 11:55:29 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ChatWithMyProject
-A RAG powered application to chat with my Master in Data Science final project!
+# Streamlit ChatBot to chat with my Master's final project!.
+
+A **RAG powered** application built using LangChain and Ollama Models. It allows you to chat with my Master's Degree final project!.
+
+Feel free to ask questions and contribute!
+
+Technologies I used:
+- `LangChain`.
+- `Streamlit` for the frontend of the application.
+- `Ollama` with llama3 model embeddings.
+- `FAISS` local vectorstore.
+
+
---
+++ rag.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import ollama
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OllamaEmbeddings
+
+PDF_PATH = "data/TFM.pdf"
+
+EMBEDDINGS = OllamaEmbeddings(model="llama3", model_kwargs={'n_gpu_layers': 0, 'n_ctx': 10000, 'n_embd': 768, 'n_head': 12, 'n_layer': 12, 'n_positions': 10000, 'vocab_size': 50256})
+
+def load_pdf(path):
+    loader = PyPDFLoader(path)
+    return loader.load()
+    
+
+def split_text(text):
+    splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100, separator= "\n") 
+    return splitter.split_documents(text)
+
+
+def build_vectorstore(splits):
+    # 1. Load and split the text
+    # 2. Create Ollama embeddings and vector store
+    
+    vectorstore = FAISS.from_documents(documents=splits, embedding=EMBEDDINGS)
+    vectorstore.save_local("./.vectorstore")
+    return print("Vectorstore created successfully!")
+
+def get_vectorstore():
+    return FAISS.load_local("./.vectorstore", embeddings=EMBEDDINGS, allow_dangerous_deserialization=True)
+
+ # 3. Call Ollama Llama3 model
+def ollama_llm(question, context):
+    formatted_prompt = f"Question: {question}\n\nContext: {context}"
+    response = ollama.chat(model='llama3', messages=[{'role': 'user', 'content': formatted_prompt}])
+    return response['message']['content']
+
+# 4. RAG Setup
+def combine_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def rag_chain(question, vectorstore):
+    retriever = vectorstore.as_retriever()
+    retrieved_docs = retriever.invoke(question)
+    formatted_context = combine_docs(retrieved_docs)
+    return ollama_llm(question, formatted_context)
+
+
+
+
+
+if __name__ == "__main__":
+    
+    data = load_pdf(PDF_PATH)
+    splits = split_text(data)
+    # build_vectorstore(splits)
+
+    vectorstore = get_vectorstore()
+    
+    # Ask a question about the webpage
+    st.title("Chat with my Final Project! 🌐")
+    st.caption("This app allows you to chat with my Final Project of my Master's Degree using local Llama-3  embeddings!")
+
+    prompt = st.text_input("Ask any question about the project")
+
+    # Chat with the webpage
+    if prompt:
+        result = rag_chain(question = prompt, vectorstore=vectorstore)
+        st.write(result)
